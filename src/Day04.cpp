f6e4f0b2cbc124b3e2486d8973a2af29ac717399
William Killian <william.killian@gmail.com>
2017-12-04 09:31:13 -0500

    [Day04] golf std::all_of the things

--- src/Day04.cpp
+++ src/Day04.cpp
@@ -1,4 +1,7 @@
 #include "Solution.hpp"
+#include <algorithm>
+#include <iterator>
+#include <numeric>
 #include <set>
 #include <sstream>
 
@@ -7,20 +10,16 @@ void
 solve<Day04>(bool part2, std::istream& is, std::ostream& os)
 {
   int sum{0};
-  for (std::string line; std::getline(is, line);) {
-    sum += [&] {
-      std::istringstream iss{line};
-      std::set<std::string> s;
-      for (std::string word; iss >> word;) {
-        if (part2) {
-          std::sort(word.begin(), word.end());
-        }
-        if (!s.insert(word).second) {
-          return 0;
-        }
-      }
-      return 1;
-    }();
+  for (std::string line; std::getline(is, line); ) {
+    std::istringstream iss{line};
+    sum += std::all_of(
+        std::istream_iterator<std::string>{iss}, {},
+        [part2, set = std::set<std::string>()] (std::string word) mutable {
+          if (part2) {
+            std::sort(word.begin(), word.end());
+          }
+          return set.insert(word).second;
+        });
   }
   os << sum << '\n';
 }
