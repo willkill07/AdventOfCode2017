550f6283cc7ea31bd5e7edc1447c16a2b33b506a
Will Killian <william.killian@gmail.com>
2017-12-05 00:24:53 -0500

    [Day05] cmp jmp incr decr

--- src/Day05.cpp
+++ src/Day05.cpp
@@ -1,8 +1,20 @@
 #include "Solution.hpp"
+#include <vector>
+#include <iterator>
 
 template <>
 void
 solve<Day05>(bool part2, std::istream& is, std::ostream& os)
 {
-
+  std::vector<int> inst {std::istream_iterator<int>{is}, {}};
+  int index{0}, steps{0}, n(inst.size());
+  while (index >= 0 && index < n) {
+    ++steps;
+    if (part2 && inst[index] >= 3) {
+      index += inst[index]--;
+    } else {
+      index += inst[index]++;
+    }
+  }
+  os << steps << '\n';
 }
