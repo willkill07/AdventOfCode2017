5835cefdbce6982bc128e71a1b735b52d6402c54
Will Killian <william.killian@gmail.com>
2017-12-06 00:55:57 -0500

    [Day05] revert clamp

--- src/Day05.cpp
+++ src/Day05.cpp
@@ -8,8 +8,8 @@ void
 solve<Day05>(bool part2, std::istream& is, std::ostream& os)
 {
   std::vector<int> inst{std::istream_iterator<int>{is}, {}};
-  int index{0}, steps{0};
-  while (std::clamp(index, 0, inst.size()) == index) {
+  int index{0}, steps{0}, n(inst.size());
+  while (index >= 0 && index < n) {
     ++steps;
     if (part2 && inst[index] >= 3) {
       index += inst[index]--;
--- src/Day06.cpp
+++ src/Day06.cpp
@@ -1,8 +1,19 @@
 #include "Solution.hpp"
+#include <algorithm>
+#include <map>
+#include <vector>
 
 template <>
 void
 solve<Day06>(bool part2, std::istream& is, std::ostream& os)
 {
-
+  std::vector<int> banks{std::istream_iterator<int>{is}, {}};
+  std::map<std::vector<int>, int> unique;
+  for (int count{0}; unique.emplace(banks, count).second; ++count) {
+    auto max = std::max_element(banks.begin(), banks.end());
+    for (int iters{std::exchange(*max, 0)}; iters--; ++*max)
+      if (++max == banks.end())
+        max = banks.begin();
+  }
+  os << unique.size() - (part2 ? unique[banks] : 0) << '\n';
 }
