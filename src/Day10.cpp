94541c1c0fea225f7d0cf1bc860a8470777ede60
Will Killian <william.killian@gmail.com>
2017-12-10 11:17:09 -0500

    [Day10] changing the locale like a disgusting human and golfing a bit

--- src/Day10.cpp
+++ src/Day10.cpp
@@ -3,33 +3,40 @@
 #include <array>
 #include <initializer_list>
 #include <iomanip>
+#include <locale>
 #include <numeric>
 #include <vector>
+#include <utility>
+
+struct csv : std::ctype<char> {
+  csv() : std::ctype<char>(get_table()) {}
+  static mask const* get_table() {
+    static mask rc[table_size];
+    rc[static_cast<int>(',')] = std::ctype_base::space;
+    rc[static_cast<int>('\n')] = std::ctype_base::space;
+    return rc;
+  }
+};
 
 template <>
 void
-solve<Day10>(bool part2, std::istream& is, std::ostream& os)
-{
+solve<Day10>(bool part2, std::istream& is, std::ostream& os) {
   std::vector<int> lengths;
   if (part2) {
-    for (char c; is >> c;)
-      lengths.push_back(c);
-    for (int i : {17, 31, 73, 47, 23})
-      lengths.push_back(i);
+    lengths.insert(lengths.end(), std::istream_iterator<char>{is}, {});
+    lengths.insert(lengths.end(), {17, 31, 73, 47, 23});
   } else {
-    int i;
-    is >> i;
-    lengths.push_back(i);
-    while (is.ignore(1, ',') >> i)
-      lengths.push_back(i);
+    is.imbue(std::locale{is.getloc(), new csv});
+    lengths.insert(lengths.end(), std::istream_iterator<int>{is}, {});
+    is.imbue(std::locale{is.getloc(), new std::ctype<char>});
   }
 
-  int const iters = part2 ? 64 : 1;
+  int iters = part2 ? 64 : 1;
   std::array<unsigned char, 256> list;
   std::iota(std::begin(list), std::end(list), 0);
 
-  unsigned char skip = 0, pos = 0;
-  for (int _{0}; _ < iters; ++_)
+  unsigned char skip{0}, pos {0};
+  while (iters--)
     for (unsigned char length : lengths) {
       std::reverse(std::begin(list), std::begin(list) + length);
       unsigned char delta = length + skip++;
@@ -39,10 +46,10 @@ solve<Day10>(bool part2, std::istream& is, std::ostream& os)
   std::rotate(std::begin(list), std::end(list) - pos, std::end(list));
 
   if (part2) {
-    auto const flags = os.flags(std::ios::hex);
+    auto const [flags, fill] = std::pair(os.flags(std::ios::hex), os.fill('0'));
     for (auto b = std::begin(list); b != std::end(list); std::advance(b, 16))
-      os << std::accumulate(b, b + 16, 0, std::bit_xor<void>());
-    os.flags(flags);
+      os << std::setw(2) << std::accumulate(b, std::next(b, 16), 0, std::bit_xor<void>());
+    os.flags(flags), os.fill(fill);
   } else {
     os << list[0] * list[1];
   }
