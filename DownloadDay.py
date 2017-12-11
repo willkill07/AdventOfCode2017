27b89b7821e4e6ab9125801b7925b6b36369e413
Will Killian <william.killian@gmail.com>
2017-12-10 22:11:07 -0500

    [General] Update download script

--- DownloadDay.py
+++ DownloadDay.py
@@ -6,20 +6,29 @@ import json
 import sys
 import pypandoc
 
-URL = 'http://adventofcode.com/2017/day/{}'
-OUT = 'Day{:02}.md'
+DESCR_URL = 'http://adventofcode.com/2017/day/{}'
+DESCR = 'descriptions/Day{:02}.md'
 
-session = dict(session='SESSION KEY GOES HERE')
+INPUT_URL = 'http://adventofcode.com/2017/day/{}/input'
+INPUT = 'inputs/Day{:02}.txt'
 
-def download(day):
-  r = requests.get(URL.format(day), cookies=session)
+session = dict(session='YOUR KEY HERE')
+
+def downloadDesc(day):
+  r = requests.get(DESCR_URL.format(day), cookies=session)
   soup = BeautifulSoup(r.text, "html5lib")
-  with open(OUT.format(day), 'w') as of:
-    for part in soup('article'):
-      of.write(pypandoc.convert_text(str(part), 'html', format='markdown'))
+  data = '\n'.join(str(p) for p in soup('article'))
+  pypandoc.convert_text(data, 'markdown', format='html', outputfile=DESCR.format(day))
+
+def downloadInput(day):
+  r = requests.get(INPUT_URL.format(day), cookies=session)
+  with open(INPUT.format(day), 'w') as of:
+    of.write(r.text)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("Usage: {} <day>".format(sys.argv[0]))
   else:
-    download(int(sys.argv[1]));
+    day = int(sys.argv[1])
+    downloadDesc(day)
+    downloadInput(day)
