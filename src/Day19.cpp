a78eb03f93b2d5528bac91cc6ae13852dab7b68a
Will Killian <william.killian@gmail.com>
2017-12-19 09:53:14 -0500

    [Day19] Remove all of the logic that is not needed

--- src/Day19.cpp
+++ src/Day19.cpp
@@ -1,4 +1,6 @@
 #include "Solution.hpp"
+#include <tuple>
+#include <utility>
 #include <vector>
 
 template <>
@@ -6,26 +8,20 @@ void
 solve<Day19>(bool part2, std::istream& is, std::ostream& os)
 {
   std::vector<std::string> grid;
-  for (std::string line; std::getline(is, line); )
+  for (std::string line; std::getline(is, line);)
     grid.push_back(line);
-  unsigned long x{grid[0].find("|")}, y{0}, count{0};
-  char dir{'D'};
-  while (grid[y][x] != ' ') {
-    ++count;
-    if (grid[y][x] == '+') {
-      if (dir == 'D' || dir == 'U') {
-        dir = (x > 0 && grid[y][x-1] != ' ') ? 'L' : 'R';
-      } else {
-        dir = (y > 0 && grid[y-1][x] != ' ') ? 'U' : 'D';
-      }
-    } else if (!part2 && std::isalpha(grid[y][x])) {
+  auto not_valid = [X = grid[0].size(), Y = grid.size()](unsigned int xx, unsigned int yy) {
+    return xx >= X || yy >= Y;
+  };
+  int count{0};
+  unsigned int x{grid[0].find("|")}, y{0};
+  for (int dx{0}, dy{1}; grid[y][x] != ' '; x += dx, y += dy, ++count) {
+    if (!part2 && std::isalpha(grid[y][x])) {
       os << grid[y][x];
-    }
-    switch (dir) {
-      case 'D': ++y; break;
-      case 'U': --y; break;
-      case 'R': ++x; break;
-      case 'L': --x; break;
+    } else if (grid[y][x] == '+') {
+      dx = -std::exchange(dy, dx);
+      if (not_valid(x + dx, y + dy) || grid[y + dy][x + dx] == ' ')
+        dx = -dx, dy = -dy;
     }
   }
   if (part2)
